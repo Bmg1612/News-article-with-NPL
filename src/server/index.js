5eac3610201f87bfd4267868c2cc9b7d0d1a1749
Bruno Gurgel <bmg1612@gmail.com>
2020-11-19 15:59:57 -0300

    feat: Configured the build-prod environment

--- src/server/index.js
+++ src/server/index.js
@@ -5,8 +5,17 @@ const apiKey = process.env.API_KEY;
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -18,8 +27,8 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
