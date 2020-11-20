809e721277acc84ea16d612692f44137edf65a4f
Bruno Gurgel <bmg1612@gmail.com>
2020-11-20 15:52:53 -0300

    feat: Connected to the API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,24 +2,41 @@ import { checkForName } from "./nameChecker"
 
 const handleSubmit = document.querySelector('#submit').addEventListener('click', function callbackFunction (event) {
     event.preventDefault();
-    // check what text was put into the form field
+
     let formText = document.getElementById('name').value;
+    const baseUrl = "https://api.meaningcloud.com/sentiment-2.1?";
+    let apiKey = "";
     // checkForName(formText);
     console.log("::: Form Submitted :::");
     getApiKey()
     .then (data => {
-        const apiKey = data.key;
+        apiKey = data.key;
+        return apiKey;
     })
+    .then (apiKey => {
+        getTextAnalysis(baseUrl, apiKey, formText)
+    });
 
     async function getApiKey () {
         let req = await fetch('http://localhost:8081/api');
         try {
             let data = await req.json();
             return data;
-        } catch (error){
+        } catch (error) {
             console.log('ERROR', error);
         }
-}
+    }
+
+    async function getTextAnalysis (url, key, text) {
+        let res = await fetch(`${url}key=${key}&of=json&txt=${text}.&model=general&lang=en`);
+        try {
+            let apiResponse = await res.json();
+            console.log(apiResponse);
+            return apiResponse;
+        } catch (error) {
+            console.log('ERROR', error);
+        }
+    }
 });
 
 export { handleSubmit }
