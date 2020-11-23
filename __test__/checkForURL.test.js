b2fb78b2aa13b835a4ed5d8da830f7d7f9002a97
Bruno Gurgel <bmg1612@gmail.com>
2020-11-23 11:29:58 -0300

    test: Finished jest testing setup

--- __test__/checkForURL.test.js
+++ __test__/checkForURL.test.js
@@ -4,5 +4,6 @@ import { checkForURL } from "../src/client/js/urlChecker"
 describe("Testing the regex functionality", () => {
     // The test() function has two arguments - a string description, and an actual test as a callback function.  
     test("Testing the checkForURL() function", () => {
-           expect(checkForURL).toBeTruthy();
+        //If returns true than it is working
+        expect(checkForURL).toBeTruthy();
 })});
