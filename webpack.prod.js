34b479456d1597a1df725a64d138a1e7fb850d76
Bruno Gurgel <bmg1612@gmail.com>
2020-11-23 15:57:51 -0300

    fix: Fixed service-worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -14,7 +15,7 @@ module.exports = {
     },
     output: {
         libraryTarget: 'var',
-        library: 'Client'
+        library: 'Client',
     },
     module: {
         rules: [
@@ -36,6 +37,18 @@ module.exports = {
         }), 
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW({
+            // these options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around
+            clientsClaim: true,
+            skipWaiting: true,
+            sourcemap: true,
+            modifyURLPrefix: {
+                'auto./': '',
+                'auto':''
+            },
+            inlineWorkboxRuntime: true   
+        }),
     ]
 }
